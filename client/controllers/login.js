7e62021a7b3a0d44e40120f4aa694ed9d2f6ea71
Alexander Charles Wolden <woldena@ochin.org>
2015-01-18 20:01:11 -0800

    added files

---
+++ client/controllers/login.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = ['$scope', 'growl',
+    function ($scope, growl) {
+
+
+
+    }
+];
---
+++ client/directives/index.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var Importer = require('module-importer');
+
+module.exports = new Importer();
